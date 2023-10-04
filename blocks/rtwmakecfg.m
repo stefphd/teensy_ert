9865274705663d2c962ed734956a34414a25e541
stefphd <stefano.lovato.1@phd.unipd.it>
2023-10-04 15:33:26 +0200

    Remove io_wrappers.cpp

--- blocks/rtwmakecfg.m
+++ blocks/rtwmakecfg.m
@@ -2,9 +2,7 @@ function makeInfo=rtwmakecfg()
 %RTWMAKECFG adds include and source directories to the generated makefiles.
 %   For details refer to documentation on the rtwmakecfg API.
 
-% Copyright 1994-2014 The MathWorks, Inc.
-
-makeInfo.sources = {'io_wrappers.cpp'};
+% makeInfo.sources = {'io_wrappers.cpp'}; NOT USED b/c using C++
 % Add the folder where this file resides to the include path
 blocks_inc_path = fileparts(mfilename('fullpath'));
 makeInfo.includePath = {blocks_inc_path};
